152a7359660a4ac884801fa8f2f61ea02ae70156
Moriom <chadninfo@gmail.com>
2023-12-29 03:31:19 +0600

    add: responsive navbar close button functionality

--- client/RAW/js/main.js
+++ client/RAW/js/main.js
@@ -1,18 +1,26 @@
 const menu_button = document.querySelector("#menu_icon_container");
 const responsive_nav_container = document.querySelector(".menu_container");
 const menu_list = document.querySelectorAll(".menu_list");
+const hamburger_icon = document.querySelector("#menu_icon_hamburger");
+const cancel_icon = document.querySelector("#menu_icon_cancel");
 
 const toggle_menu = () => {
   if (responsive_nav_container.classList.contains("show_menu_container")) {
     responsive_nav_container.classList.remove("show_menu_container");
+    hamburger_icon.classList.remove("menu_icon_transform");
+    cancel_icon.classList.add("menu_icon_transform");
   } else {
-    console.log(responsive_nav_container.classList);
+    cancel_icon.classList.remove("menu_icon_transform");
+    hamburger_icon.classList.add("menu_icon_transform");
     responsive_nav_container.classList.add("show_menu_container");
   }
 };
 
 menu_list.forEach((list) => {
-  list.addEventListener("click", toggle_menu);
+  if (list.classList.contains("menu_list"))
+    list.addEventListener("click", toggle_menu);
 });
+
+cancel_icon.addEventListener("click", toggle_menu);
 
 menu_button.addEventListener("click", toggle_menu);
