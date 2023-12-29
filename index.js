0cfe1c6086c94aa659979afb4dd8ae696edd7bda
Moriom <chadninfo@gmail.com>
2023-12-30 04:08:42 +0600

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const PORT = process.evn.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client")));
 
