82e3c841f5ded750e55716390f74a575b149a388
Ibrhaim Hossain <mohammad.ihakash@gmail.com>
2023-12-31 03:46:10 +0600

    add: User schema

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    trim: true,
+    require: true,
+  },
+  email: {
+    type: String,
+    maxLength: 128,
+    lowercase: true,
+    dropDups: true,
+    unique: true,
+    index: true,
+    trim: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "INVALID EMAIL"],
+    required: [true, "EMAIL IS REQUIRED"],
+  },
+  hashPassword: {
+    type: String,
+    required: [true, "PASSWORD IS REQUIRED"],
+  },
+  role: {
+    type: String,
+    lowercase: true,
+    enum: ["admin", "editor", "user"],
+    default: "user",
+  },
+  salt: {
+    type: Number,
+    default: process.env.SALT,
+  },
+  iat: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+userSchema.path("email").validate(async (email) => {
+  const emailCount = await mongoose.models.User.countDocuments({ email });
+  return !emailCount;
+}, "EMAIL ALREADY EXISTS!");
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
