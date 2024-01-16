fdbf30fe05ae3fcc9cf702bbdc4dc3aa36520bde
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-01-16 16:37:45 +0600

    add: onclick result

--- client/js/main.js
+++ client/js/main.js
@@ -3,6 +3,10 @@ const responsive_nav_container = document.querySelector(".menu_container");
 const menu_list = document.querySelectorAll(".menu_list");
 const hamburger_icon = document.querySelector("#menu_icon_hamburger");
 const cancel_icon = document.querySelector("#menu_icon_cancel");
+const searchBox = document.querySelector(".search-box");
+const searchResultContainer = document.querySelector(
+  ".realtime-search-result-container"
+);
 
 const toggle_menu = () => {
   if (responsive_nav_container.classList.contains("show_menu_container")) {
@@ -16,6 +20,45 @@ const toggle_menu = () => {
   }
 };
 
+const fetchAndSuggestResult = async (e) => {
+  const keyword = e.target.value.trim();
+  if (keyword.length > 0) {
+    const fetch_data = await fetch(`/api/medicine/search/${keyword}`);
+    const data = await fetch_data.json();
+    if (data?.length > 0) processDataInSearchResult(data);
+  } else {
+    searchResultContainer.innerHTML = ``;
+  }
+};
+
+const processDataInSearchResult = (data) => {
+  searchResultContainer.innerHTML = ``;
+  try {
+    data?.forEach((med) => {
+      const medCart = document.createElement("div");
+      medCart.setAttribute("key", med?._id);
+      medCart.classList.add("search-med-cart");
+      medCart.onclick = handleOnClickResult;
+      medCart.innerHTML = `
+        <p>${med?.name} <span class="medicine-strength">${med?.strength}</span></p>
+        <p class="search-result-common-text medicine-type">${med?.type}</p>
+        <p class="search-result-common-text">${med?.generic}</p>
+        <p class="search-result-common-text">${med?.company}</p>
+    `;
+      searchResultContainer.appendChild(medCart);
+    });
+  } catch {}
+};
+
+const handleOnClickResult = (e) => {
+  const parentElement = e.target.parentElement;
+  const attrsName = parentElement.getAttributeNames();
+  let ID = "";
+  attrsName.forEach((at) => {
+    if (at === "key") ID = parentElement.getAttribute(at);
+  });
+};
+
 menu_list.forEach((list) => {
   if (list.classList.contains("menu_list"))
     list.addEventListener("click", toggle_menu);
@@ -24,3 +67,5 @@ menu_list.forEach((list) => {
 cancel_icon.addEventListener("click", toggle_menu);
 
 menu_button.addEventListener("click", toggle_menu);
+
+searchBox.addEventListener("input", fetchAndSuggestResult);
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,16 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-mongoose.connect("mongodb://127.0.0.1:27017", {
-    dbName: "medseek",
-  })
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.NODE_ENV === "DEVELOPMENT"
+      ? "mongodb://127.0.0.1:27017"
+      : "mongodb+srv://medAdmin:med123123@cluster0.5p7yt.mongodb.net",
+    {
+      dbName: "medseek",
+    }
+  )
   .then((data) => console.log("DB connected"))
   .catch((e) => console.error(e));
