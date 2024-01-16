4d950ff1fc8593ed201e05c62319fe5b5591877f
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-01-16 16:41:43 +0600

    udpate

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const authRouter = require("./routers/authRouter");
 const medRouter = require("./routers/medecineRouter");
 require("./config/dbConnection");
@@ -10,6 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "client")));
